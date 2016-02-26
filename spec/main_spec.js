7ffe6c53e178be1f72ce22fe2f37c3dd51d89887
dengyangself <dengyangself@gmail.com>
2016-02-26 13:58:53 +0800

    Update main.js for checking inputs.

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -9,14 +9,28 @@ chai.use(sinonChai);
 var main = require("../lib/main.js");
 
 var base_digit = [3, 5, 7];
-var base_digit_wrong = [3, 5, 3];
+var base_digit_duplicate = [3, 5, 3];
+var base_digit_over_range = [3, 5, 17];
+var base_digit_over_3 = [3, 5, 7, 8];
 
 describe("测试描述", function() {
   sinon.spy(console, 'log');
 
-  it("wrong_base_digits", function() {
-    var result = main(base_digit_wrong, 3);
-    var expect_string = 'wrong base digits';
+  it("wrong_base_digits_duplicate", function() {
+    var result = main(base_digit_duplicate, 3);
+    var expect_string = 'wrong base digits - duplicate';
+    expect(expect_string).to.equal(result);
+  });
+
+  it("wrong_base_digits_ovre_range", function() {
+    var result = main(base_digit_over_range, 3);
+    var expect_string = 'wrong base digits - over range';
+    expect(expect_string).to.equal(result);
+  });
+
+  it("wrong_base_digits_over_3", function() {
+    var result = main(base_digit_over_3, 3);
+    var expect_string = 'wrong base digits - more than 3 digits';
     expect(expect_string).to.equal(result);
   });
 
@@ -26,15 +40,15 @@ describe("测试描述", function() {
     expect(expect_string).to.equal(result);
   });
 
-  it("digit_6", function() {
-    var result = main(base_digit, 6);
-    var expect_string = '1\n2\nFizz\n4\nBuzz\nFizz\n';
+  it("digit_5", function() {
+    var result = main(base_digit, 5);
+    var expect_string = '1\n2\nFizz\n4\nBuzz\n';
     expect(expect_string).to.equal(result);
   });
 
-  it("digit_8", function() {
-    var result = main(base_digit, 8);
-    var expect_string = '1\n2\nFizz\n4\nBuzz\nFizz\nWhizz\n8\n';
+  it("digit_7", function() {
+    var result = main(base_digit, 7);
+    var expect_string = '1\n2\nFizz\n4\nBuzz\nFizz\nWhizz\n';
     expect(expect_string).to.equal(result);
   });
 
